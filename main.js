86b7cf63af9808691d2cb35be35e8c76950be06a
krishlala <71400038+krishlala@users.noreply.github.com>
2021-09-25 11:11:51 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function book() {
+    window.location = "book.html";
+}
+
+function bottle() {
+    window.location = "bottle.html";
+}
+
+function phone() {
+    window.location = "phone.html";
+}
+
+function apple() {
+    window.location = "apple.html";
+}
+
+function notebook() {
+    window.location = "notebook.html";
+}
